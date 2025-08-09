33f33428e9189dd16625146dfa7281c773c37699
wenfuras1 <wenfurnas@alu.ufc.br>
2025-08-08 22:44:03 -0300

    primeiro test de project nagila

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 function toggleMode() {
     const html = document.documentElement
+    html.classList.toggle('light')
 
-
-    html.classList.toggle("light")
-
-
-    const img = document.querySelector("#profile img")
+    const img = document.querySelector('#profile img')
 
     if (html.classList.contains('light')) {
-        img.setAttribute('src', './assets/avatar-light.png')
-
+        img.setAttribute('src', './nagila.jpeg')
+        img.setAttribute('alt', 'Foto de nagila lima tiktoker')
     } else {
-        img.setAttribute('src', './assets/avatar.png')
-
+        img.setAttribute('src', './nagila2.jpg')
+        img.setAttribute('alt', 'Foto de nagila lima fisioterapeuta')
     }
-
 }
